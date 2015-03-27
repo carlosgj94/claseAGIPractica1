b2dac180fca72508986f734328cfcbea9369ccae
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2015-03-27 21:00:58 +0100

    Vaya, parece que no funciona del todo, pero casi que ya

--- Pregunta2.sql
+++ Pregunta2.sql
@@ -1,5 +1,5 @@
 #select nombre, apellidos
-select nombre, apellidos from jugador
+select nombre, apellidos, TORNEO_idTorneo from jugador
 	join inscritos_torneo
     on jugador.idJugador= inscritos_torneo.JUGADOR_idJugador
 	and jugador.banco is null;
