b9f96c58ecb69164030b8ddc4f582c7b1ce421f8
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2015-03-29 20:15:24 +0200

    Comentario de que faltan por mostrar los que no tienen amigos

--- Pregunta4.sql
+++ Pregunta4.sql
@@ -1,4 +1,13 @@
-select idJugador, count(*) from jugador, amistad
-	where JUGADOR_idJugador = idJugador
-    or JUGADOR_idJugador1 = idJugador
-		group by JUGADOR_idJugador, JUGADOR_idJugador1;
+#Falta por mostrar a todos aquellos que no tengan ningun amigo
+
+select 
+	idJugador, 
+    count(JUGADOR_idJugador & JUGADOR_idJugador1) as NumAmigos
+from 
+	jugador, 
+    amistad
+where
+	JUGADOR_idJugador = idJugador or
+    JUGADOR_idJugador1 = idJugador
+group by
+	idJugador;
