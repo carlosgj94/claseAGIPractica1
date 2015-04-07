7ab7d8d1a19da081f048df4734a62dbd9326f538
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2015-04-07 20:46:16 +0200

    Corregido un pequenyo error del 3 de la fecha. Ademas hecho el 7 en perfectas condiciones (aunque feo)

--- Pregunta3.sql
+++ Pregunta3.sql
@@ -1,4 +1,4 @@
 select nombre from torneo 
-	where torneo.fechaCelebracion between "20150101" and "20160101"
+	where torneo.fechaCelebracion between "20150101" and "20151231"
     order by premio desc limit 1;
 	
--- Pregunta7.sql
+++ Pregunta7.sql
@@ -1,1 +1,20 @@
-select * from pareja
+SELECT 
+    alias, SUM(premio) AS premios
+FROM
+    jugador
+        JOIN
+    (SELECT 
+        jugador_idjugador1, jugador_idjugador2, premio
+    FROM
+        pareja
+    JOIN (SELECT 
+        idPareja1, idPareja2, SUM(premio/2) AS premio
+    FROM
+        partida
+    WHERE
+        torneo_idtorneo IS NULL
+    GROUP BY idPareja1 , idPareja2) AS Ppremio ON pareja.idPareja = ppremio.idPareja1
+        OR pareja.idPareja = ppremio.idPareja2) AS jpremio ON jugador.idjugador = jpremio.jugador_idjugador1
+        OR jugador.idjugador = jpremio.jugador_idjugador2
+GROUP BY idJugador
+order by premios limit 1;
