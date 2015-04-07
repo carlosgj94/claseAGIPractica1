4e7fe9a53bce0ad6fa964d5609408dbacbd8479c
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2015-04-07 16:46:23 +0200

    Corregimos los errores de la query 2 y 4

--- Pregunta2.sql
+++ Pregunta2.sql
@@ -1,6 +1,11 @@
-#Muestra el nombre de todos los torneos y no solo del que toca
-
-select jugador.nombre, apellidos, torneo.nombre from torneo ,jugador
-	join inscritos_torneo
-    on jugador.idJugador= inscritos_torneo.JUGADOR_idJugador
-	and jugador.banco is null;
+SELECT 
+    jugnombre, apellidos, torneo.nombre
+FROM
+    torneo
+        JOIN
+    (SELECT 
+        nombre AS jugNombre, apellidos, torneo_idtorneo AS idTorneo
+    FROM
+        jugador
+    JOIN inscritos_torneo ON jugador.idjugador = inscritos_torneo.jugador_idjugador
+        AND jugador.banco IS NULL) AS jugT ON torneo.idTorneo = jugT.idTorneo;
--- Pregunta4.sql
+++ Pregunta4.sql
@@ -1,13 +1,10 @@
-#Falta por mostrar a todos aquellos que no tengan ningun amigo
-
-select 
-	idJugador, 
-    count(JUGADOR_idJugador & JUGADOR_idJugador1) as NumAmigos
-from 
-	jugador, 
-    amistad
-where
-	JUGADOR_idJugador = idJugador or
-    JUGADOR_idJugador1 = idJugador
-group by
-	idJugador;
+SELECT 
+    idJugador,
+    COUNT(JUGADOR_idJugador & JUGADOR_idJugador1) AS NumAmigos
+FROM
+    jugador
+        LEFT JOIN
+    amistad ON JUGADOR_idJugador = idJugador
+        OR JUGADOR_idJugador1 = idJugador
+GROUP BY idJugador
+ORDER BY numamigos DESC;
