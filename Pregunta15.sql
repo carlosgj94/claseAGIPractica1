6df3a8b5d310e04de591ebca33dd5d81c6414004
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2015-04-08 15:25:47 +0200

    Trigger del 15 hecho

--- Pregunta15.sql
+++ Pregunta15.sql
@@ -1,12 +1,19 @@
-#Aun no funciona, pero tengo suenyo
-delimiter 
-create trigger ParejasDistintas before insert on partida
+delimiter //
+create trigger ParejasDistintasInsert before insert on partida
+	for each row
+    begin
+	if new.idpareja1 = new.idpareja2 then
+        signal sqlstate "45000" set message_text = "Las parejas han de ser distintas";
+	end if;
+end;
+delimiter //
+
+delimiter //
+create trigger ParejasDistintasUpdate before update on partida
 	for each row
     begin
 	if new.idpareja1 = new.idpareja2 then
         signal sqlstate "45001" set message_text = 'Las parejas han de ser distintas';
-    else
-		update partida
-	end if
+	end if;
 end;
-delimiter;
+delimiter //
