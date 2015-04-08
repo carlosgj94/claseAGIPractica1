af9dfa61e559e09fcf7877bcbd7821420eea42c8
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2015-04-08 18:11:19 +0200

    Practica terminada. :D

--- Pregunta11.sql
+++ Pregunta11.sql
@@ -1,2 +1,30 @@
-#No entiedno la estructura de la base de datos
-select * from jugada
+SELECT 
+    *
+FROM
+    (SELECT 
+        jugada_idjugada
+    FROM
+        detalles_jugadas
+    WHERE
+        carta_idcarta IN (SELECT 
+                idcarta
+            FROM
+                carta
+            WHERE
+                nombre LIKE 'rey%' OR nombre LIKE '3%')
+    GROUP BY jugada_idjugada
+    HAVING COUNT(jugada_idjugada) = 2) AS jugadaRey
+WHERE
+    jugada_idjugada IN (SELECT 
+            jugada_idjugada
+        FROM
+            detalles_jugadas
+        WHERE
+            carta_idcarta IN (SELECT 
+                    idcarta
+                FROM
+                    carta
+                WHERE
+                    nombre LIKE 'as%' OR nombre LIKE '2%')
+        GROUP BY jugada_idjugada
+        HAVING COUNT(jugada_idjugada) = 2)
